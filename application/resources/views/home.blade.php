d4344bdb163b242fc2a17a33f920949d9329c7f2
Shiro_nekos <felisgosal1996@gmail.com>
2022-07-02 20:24:50 +0700

    setup awal laravel dan vue2

---
+++ application/resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title> Program SPK</title>
+    <style>
+        body{
+            font-family: sans-serif;
+            margin: 15%
+        }
+    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+        <div id="app">
+                <h3>hai laravel</h3>
+        </div>
+
+</body>
+</html>
