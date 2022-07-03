16eca2984aed24b4e91cc54609bef476cbe88743
CHARLES250601 <44624179+CHARLES250601@users.noreply.github.com>
2022-07-03 22:31:48 +0700

    tampilan input SPK selesai

--- application/resources/js/components/Pages/InputNoSPK/InputSPKComponent.vue
+++ application/resources/js/components/Pages/InputNoSPK/InputSPKComponent.vue
@@ -6,7 +6,7 @@
                 <div class="card">
                     <div class="card-header">
                         <span class="adiputro-wirasejati">
-                        <b><center>PT Adiputro Wirasejati</center></b>
+                        <b><center>INPUT NOMOR SPK</center></b>
                         </span>
                     </div>
 
@@ -15,14 +15,83 @@
                             <div class="row">
                                 <div class="col-8 tabel-data-spk">
                                     <div class="container">
-                                        <form>
-                                            <div class="form-group row">
-                                                <label class="col col-form-label">Tanggal SPK</label>
-                                                <div class="col">
-                                                    <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+                                        <div class="row">
+                                            <div class="col-sm">
+                                                <form>
+                                                    <div class="form-group row">
+                                                        <label class="col col-form-label">Tanggal SPK</label>
+                                                        <div class="col">
+                                                            <input type="text" class="form-control" id="inputPassword" placeholder="Masukkan tanggal (nanti di ganti date time picker)">
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="form-group row">
+                                                        <label class="col col-form-label">Nama</label>
+                                                        <div class="col">
+                                                            <input type="text" class="form-control" id="inputPassword" placeholder="Nama">
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="form-group row">
+                                                        <label class="col col-form-label">Alamat</label>
+                                                        <div class="col">
+                                                            <input type="text" class="form-control" id="inputPassword" placeholder="Alamat">
+                                                        </div>
+                                                    </div>
+
+                                                </form>
+                                            </div>
+                                            <div class="col-sm">
+                                                <div class="form-group row">
+                                                    <label class="col col-form-label">No. SPK</label>
+                                                    <div class="col">
+                                                        <input type="text" class="form-control" id="inputPassword" placeholder="Nomor SPK">
+                                                    </div>
+                                                </div>
+
+                                                <div class="form-group row">
+                                                    <label class="col col-form-label">Tanggal Penerimaan</label>
+                                                    <div class="col">
+                                                        <input type="text" class="form-control" id="inputPassword" placeholder="Masukkan tanggal penerimaan (nanti di ganti date time picker)">
+                                                    </div>
+                                                </div>
+
+                                                <div class="form-group row"> 
+                                                    <label class="col col-form-label">Status</label>
+                                                    <div class="col">
+                                                        <input type="text" class="form-control" id="inputPassword" placeholder="Masukkan status">
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </form>
+                                        </div>
+                                        <br>
+                                        <div class="row">
+                                            <div class="col border border-dark">
+                                                <div class="row">Merk</div>
+                                                <div class="row">ISUZU</div>
+                                            </div>
+                                            <div class="col border border-dark">
+                                                <div class="row">Type / Model</div>
+                                                <div class="row">NLR55BLX</div>
+                                            </div>
+                                            <div class="col border border-dark">
+                                                <div class="row">Tahun</div>
+                                                <div class="row">2022</div>
+                                            </div>
+                                            <div class="col border border-dark">
+                                                <div class="row">No Serie</div>
+                                                <div class="row"></div>
+                                            </div>
+                                            <div class="col border border-dark">
+                                                <div class="row">No Rangka</div>
+                                                <div class="row">MHCNLR55</div>
+                                            </div>
+                                            <div class="col border border-dark">
+                                                <div class="row">Header</div>
+                                                <div class="row">isi</div>
+                                            </div>
+                                        </div>
+                                        
                                     </div>
 
                                 </div>
@@ -58,10 +127,10 @@
     font-size: 3em;
 }
 .tabel-data-spk {
-    background-color: lightgreen;
+    /* background-color: lightgreen; */
 }
 .button-spk{
-    background-color: aqua;
+    /* background-color: aqua; */
 }
 .button-input{
     width: 70%;
