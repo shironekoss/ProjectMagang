922968594209a5f765a4eed0c0fb77058e13986d
CHARLES250601 <44624179+CHARLES250601@users.noreply.github.com>
2022-07-04 14:18:08 +0700

    tampilan

--- application/resources/js/components/General/FooterComponent.vue
+++ application/resources/js/components/General/FooterComponent.vue
@@ -1,5 +1,41 @@
 <template>
-    <footer>
-        <p>&copy; 2022</p>
+    <!-- Footer -->
+    <footer
+        class="text-center text-lg-start text-white"
+        style="background-color: #3e4551">
+        <!-- Grid container -->
+        <div class="container p-4 pb-0">
+            <!-- Section: Links -->
+            <section class="">
+                <!--Grid row-->
+                <div class="row">
+                    <!--Grid column-->
+                    <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+                        <h5 class="text-uppercase">PT. Adiputro Wirasejati</h5>
+                        <p>
+                            Jl. Raya Balearjosari No. 35 <br>
+                            Malang, Jawa Timur, 65126 <br>
+                        </p>
+                        
+
+                    </div>
+                    <!--Grid column-->
+                    <div class="col">
+                    </div>
+                    <!--Grid column-->
+                </div>
+                <!--Grid row-->
+            </section>
+            <!-- Section: Links -->
+        </div><br>
+        <!-- Grid container -->
+
+        <!-- Copyright -->
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+            © 2022 Copyright:
+            <a class="text-white" href="https://adiputrogroup.com/" target="_blank">PT. Adiputro Wirasejati</a >
+        </div>
+        <!-- Copyright -->
     </footer>
+    <!-- Footer -->
 </template>
