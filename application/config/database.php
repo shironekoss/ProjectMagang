d343356a8335568211d86d81053b1f0d0f2d35ef
Shiro_nekos <felisgosal1996@gmail.com>
2022-07-15 23:05:52 +0700

    add font awesome

--- application/config/database.php
+++ application/config/database.php
@@ -43,9 +43,6 @@ return [
             'username' => env('DB_USERNAME', 'homestead'),
             'password' => env('DB_PASSWORD', 'secret'),
             'options' => [
-                // here you can pass more settings to the Mongo Driver Manager
-                // see https://www.php.net/manual/en/mongodb-driver-manager.construct.php under "Uri Options" for a list of complete parameters that you can use
-        
                 'database' => env('DB_AUTHENTICATION_DATABASE', 'admin'), // required with Mongo 3+
             ],
         ],
