7f628553009316427da72ec9f9c0b1bfed949261
Shiro_nekos <felisgosal1996@gmail.com>
2022-09-11 23:34:39 +0700

    nambah model departemen

--- application/resources/js/components/Credential/Login.vue
+++ application/resources/js/components/Credential/Login.vue
@@ -12,7 +12,7 @@
                         <div v-if="!registerActive" class="card login" v-bind:class="{ error: emptyFields }">
                             <h1>Sign In</h1>
                             <form class="form-group">
-                                <input v-model="emailLogin" type="email" class="form-control" placeholder="Email"
+                                <input v-model="usernameLogin" type="text" class="form-control" placeholder="Username"
                                     required>
                                 <input v-model="passwordLogin" type="password" class="form-control"
                                     placeholder="Password" required>
@@ -23,11 +23,10 @@
                                 <p><a href="#">Forgot your password?</a></p>
                             </form>
                         </div>
-
                         <div v-else class="card register" v-bind:class="{ error: emptyFields }">
                             <h1>Sign Up</h1>
                             <form class="form-group">
-                                <input v-model="emailReg" type="email" class="form-control" placeholder="Email"
+                                <input v-model="emailReg" type="text" class="form-control" placeholder="Username"
                                     required>
                                 <input v-model="passwordReg" type="password" class="form-control" placeholder="Password"
                                     required>
@@ -41,87 +40,123 @@
                         </div>
                     </div>
                 </div>
-
             </div>
         </div>
     </div>
 </template>
-<script></script>
+<script>
+export default {
+    data() {
+        return {
+            registerActive: false,
+            usernameLogin: "",
+            passwordLogin: "",
+            emailReg: "",
+            passwordReg: "",
+            confirmReg: "",
+            emptyFields: false
+        }
+    },
+    methods: {
+        async doLogin() {
+            if (this.usernameLogin === "" || this.passwordLogin === "") {
+                this.emptyFields = true;
+            } else {
+
+                await axios.post('/api/login', {username:this.usernameLogin,password:this.passwordLogin}).then((response) => {
+                    
+                });
+                alert("You are now logged in");
+            }
+        },
+
+        doRegister() {
+            if (this.emailReg === "" || this.passwordReg === "" || this.confirmReg === "") {
+                this.emptyFields = true;
+            } else {
+                alert("You are now registered");
+            }
+        }
+    }
+}
+</script>
 
 <style lang="scss">
 p {
-   line-height: 1rem;
+    line-height: 1rem;
 }
 
 .card {
-   padding: 20px;
+    padding: 20px;
 }
 
 .form-group {
-   input {
-      margin-bottom: 20px;
-   }
+    input {
+        margin-bottom: 20px;
+    }
 }
 
 .login-page {
-   align-items: center;
-   display: flex;
-   height: 100vh;
+    align-items: center;
+    display: flex;
+    height: 100vh;
 
-   .wallpaper-login {
-      background: url(https://images.pexels.com/photos/32237/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260)
-         no-repeat center center;
-      background-size: cover;
-      height: 100%;
-      position: absolute;
-      width: 100%;
-   }
+    .wallpaper-login {
+        background: url(https://images.pexels.com/photos/32237/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260) no-repeat center center;
+        background-size: cover;
+        height: 100%;
+        position: absolute;
+        width: 100%;
+    }
 
-   .fade-enter-active,
-   .fade-leave-active {
-  transition: opacity .5s;
-}
-   .fade-enter,
-   .fade-leave-to {
-      opacity: 0;
-   }
+    .fade-enter-active,
+    .fade-leave-active {
+        transition: opacity .5s;
+    }
 
-   .wallpaper-register {
-      background: url(https://images.pexels.com/photos/533671/pexels-photo-533671.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260)
-         no-repeat center center;
-      background-size: cover;
-      height: 100%;
-      position: absolute;
-      width: 100%;
-      z-index: -1;
-   }
+    .fade-enter,
+    .fade-leave-to {
+        opacity: 0;
+    }
 
-   h1 {
-      margin-bottom: 1.5rem;
-   }
+    .wallpaper-register {
+        background: url(https://images.pexels.com/photos/533671/pexels-photo-533671.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260) no-repeat center center;
+        background-size: cover;
+        height: 100%;
+        position: absolute;
+        width: 100%;
+        z-index: -1;
+    }
+
+    h1 {
+        margin-bottom: 1.5rem;
+    }
 }
 
 .error {
-   animation-name: errorShake;
-   animation-duration: 0.3s;
+    animation-name: errorShake;
+    animation-duration: 0.3s;
 }
 
 @keyframes errorShake {
-   0% {
-      transform: translateX(-25px);
-   }
-   25% {
-      transform: translateX(25px);
-   }
-   50% {
-      transform: translateX(-25px);
-   }
-   75% {
-      transform: translateX(25px);
-   }
-   100% {
-      transform: translateX(0);
-   }
-}
+    0% {
+        transform: translateX(-25px);
+    }
 
+    25% {
+        transform: translateX(25px);
+    }
+
+    50% {
+        transform: translateX(-25px);
+    }
+
+    75% {
+        transform: translateX(25px);
+    }
+
+    100% {
+        transform: translateX(0);
+    }
+}
 </style>
