a222b38b30175ed218c0f0bc966f17a75032109e
Shiro_nekos <felisgosal1996@gmail.com>
2022-12-04 21:08:58 +0700

    v.1.0.2

--- application/routes/web.php
+++ application/routes/web.php
@@ -18,10 +18,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/testspk',[SPKController::class,'filternospk']);
 Route::get('/latihan',[SPKController::class,'latihan']);
 
