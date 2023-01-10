c7aa01d167d399bb437234e2165f6935c8fde55b
Shiro_nekos <felisgosal1996@gmail.com>
2023-01-10 21:12:35 +0700

    buku kp bab 1-6 kecuali 4 siap

--- application/app/Models/Account.php
+++ application/app/Models/Account.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Jenssegers\Mongodb\Eloquent\Model;
 use Jenssegers\Mongodb\Auth\User as Authenticatable;
@@ -9,19 +7,12 @@ use Illuminate\Notifications\Notifiable;
 use Jenssegers\Mongodb\Eloquent\SoftDeletes;
 use Laravel\Sanctum\HasApiTokens;
 
-
-
 class Account extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
-
     protected $connection = 'mongodb';
     protected $collection = 'Accounts_DB';
-    // protected $table        = "accounts";
-    // protected $primaryKey   = "account_id";
-    // public $incrementing    = true;
     public $timestamps      = true;
-
     protected $fillable = [
         'account_name',
         'account_username',
